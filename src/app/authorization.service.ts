59ff135715c7e4714094eeb78e456080dbf82486
Vladyslav Sazonov <vladyslavsazonov@MBP-Vladyslav.dinarys.net>
2018-03-20 09:21:57 +0200

    authorization v0.2

--- src/app/authorization.service.ts
+++ src/app/authorization.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AuthorizationService {
 
-  private isRegistered = false;
+  public isRegistered = true;
 
   constructor() { }
 
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthorizationService } from '../authorization.service';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navbar',
@@ -8,9 +9,33 @@ import { AuthorizationService } from '../authorization.service';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private auth: AuthorizationService) { }
+  private closeResult: string;
+  private isRegistered;
+
+  constructor(private auth: AuthorizationService, private modalService: NgbModal) { }
 
   ngOnInit() {
+    this.isRegistered = this.auth.isRegistered;
+  }
+
+  open(content) {
+    console.log(this.isRegistered);
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
 }
